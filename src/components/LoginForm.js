7882ad5dd7eebf984ff0df0aef1dc748ec8a3a2f
jasperwreed <jmixon.reed@gmail.com>
2020-11-02 11:45:47 -0800

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,36 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.username.length && this.state.password.length){
+      this.props.handleLogin({username: this.state.username, password:this.state.password})
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => this.handleSubmit(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={e => this.handleInputChange(e)}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={e => this.handleInputChange(e)}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,32 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
+
+  handleChange = (e) => {
+    if (e.target.value.length <= this.props.maxChars){
+      this.setState({message: e.target.value})
+    }
+  }
+
+  remainingChars = () => {
+    if (this.state.message.length < this.props.maxChars-25){
+      return <span>{this.props.maxChars - this.state.message.length}</span>
+    } else {
+      return <span style={{color: 'red'}}>{this.props.maxChars - this.state.message.length}</span>
+    }
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={(e) => this.handleChange(e)} />
+        {this.remainingChars()}
       </div>
     );
   }
